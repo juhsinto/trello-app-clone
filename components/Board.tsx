9e79dbaad0f14ac86644069b5a1e21fde0eb7846
Jacinto Mendes <jacinto.mendes@ciandt.com>
2023-06-08 14:32:12 +1000

    1:24:03 - implementing todo data

---
+++ components/Board.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useBoardStore } from "@/store/BoardStore";
+import React, { useEffect } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+function Board() {
+  const getBoard = useBoardStore((state) => state.getBoard);
+
+  useEffect(() => {
+    getBoard();
+  }, [getBoard]);
+
+  return (
+    <h1>hello</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal">
+    //     {(provided) => <div>{/* rendering all the columns */}</div>}
+    //   </Droppable>
+    // </DragDropContext>
+  );
+}
+
+export default Board;
